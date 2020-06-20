4cd548741b194c6b9a7898dc8ba0a0c42ec7ec3a
ye-yu <rafolwen98@gmail.com>
2020-06-20 15:32:27 +0800

    Fetch input values

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 "use strict";
+/*
+Classes
+*/
+class TextInput {
+  constructor(source, defValue, isTextArea) {
+    if (isTextArea){
+      this.getValue = () => source.html();
+    } else {
+      this.getValue = () => source.val();
+    }
+    this.def = defValue;
+  }
+  getOrDefault() {
+    const val = this.getValue();
+    if (val.length == 0) return def;
+    return val;
+  }
+}
 
+class IntInput extends TextInput{
+  constructor(source, defValue, isTextArea) {
+    super(source, defValue, isTextArea);
+  }
+
+  getOrDefault() {
+    const val = this.getValue();
+    if (val.length == 0 || Number.isNaN(val)) return +this.def;
+    return +val;
+  }
+}
+
+/*
+Constants
+*/
 const BUTTONS = {
   START: $("#start-rolling"),
   STOP: $("#stop-rolling")
 }
+
+const ENTRIES = {
+  TEXT: new TextInput($("#input-text"), "", true),
+  DELAY: new IntInput($("#delay"), "5", false),
+  WPM: new IntInput($("#wpm"), "200", false)
+}
+
+/*
+Functions
+*/
 function init() {
   // register event handlers
   BUTTONS.START.on('click', clickStartRolling);
@@ -14,16 +57,19 @@ function init() {
 }
 
 function clickStartRolling() {
-  console.log("Start rolling texts");
+  console.log("Delay before start: ", ENTRIES.DELAY.getOrDefault());
+  console.log("Words per minute: ", ENTRIES.WPM.getOrDefault());
+  console.log("Text to play: ", ENTRIES.TEXT.getValue());
   BUTTONS.START.hide();
   BUTTONS.STOP.show();
 }
 
 function clickStopRolling() {
-  console.log("Stop rolling texts");
   BUTTONS.STOP.hide();
   BUTTONS.START.show();
 }
 
-
+/*
+Initialisation
+*/
 init();
